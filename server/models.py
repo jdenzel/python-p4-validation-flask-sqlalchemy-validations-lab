aa32b691d3593579518717483e36e8c9014a6654
Denzel <jdenzelmab@gmail.com>
2023-09-19 19:18:10 -0400

    Completed validation flask sqlalchemy validations lab

--- server/models.py
+++ server/models.py
@@ -8,10 +8,23 @@ class Author(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name= db.Column(db.String, unique=True, nullable=False)
-    phone_number = db.Column(db.String)
+    phone_number = db.Column(db.String, db.CheckConstraint('phone_number > 10'))
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
+    @validates('name')
+    def validate_name(self, key, name):
+        if len(name) > 0:
+            return name
+        else:
+            raise ValueError
+
+    @validates('phone_number')
+    def validate_pn(self, key, phone_number):
+        if len(phone_number) > 10:
+            raise ValueError
+        return phone_number
+        
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
 
@@ -27,6 +40,43 @@ class Post(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
+    @validates('content')
+    def validate_content(self, key, content):
+        if len(content) < 250:
+            raise ValueError
+        return content
+    
+    @validates('summary')
+    def validate_summary(self, key, summary):
+        if len(summary) > 250:
+            raise ValueError
+        return summary
+        
+    @validates('category')
+    def validate_category(self, key, category):
+        if category == 'Fiction':
+            return category
+        else:
+            if category == 'Non-Fiction':
+                return category
+        raise ValueError
+    
+    @validates('title')
+    def validate_title(self, key, title):
+        if title == "Won't Believe":
+            return title
+        else:
+            if title == 'Secret':
+                return title
+            else:
+                if title == 'Top':
+                    return title
+                else:
+                    if title == 'Guess':
+                        return title
+        raise ValueError
+        
+    
 
     def __repr__(self):
         return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
